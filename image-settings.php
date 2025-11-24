33b3c67f38de805fd3a7f7da1a69c4455365c0e5
Harshdeep <harshdeep@glocify.com>
2025-11-24 13:48:48 +0530

    Revert "accessibyte"
    This reverts commit 7862150199863d04cb1cfdc7776bf34e611195ee.

--- image-settings.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-//Login | Forgot | Regestration page image will set here
-define( 'LOGIN_PAGE_IMG', ADMIN_URL.'img/login-forgot-register-bg.JPG');
-
-//Logout page spiner image will set here
-define( 'LOGOUT_SPINNER', '');
-
-?>
